30f7699bf69bd2939efdb3ba67cd2e28fc8b2d17
Plinio-pro <jossueloor25@gmail.com>
2024-12-22 18:09:20 -0500

    Init: Sistema Facturación project

---
+++ SistemaFacturacion/src/ec/edu/puce/facturacion/Main.java
@@ -0,0 +1,10 @@
+package ec.edu.puce.facturacion;
+
+import ec.edu.puce.forms.FrmMenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        FrmMenuPrincipal mainMenu = new FrmMenuPrincipal();
+        mainMenu.setVisible(true);
+    }
+}
